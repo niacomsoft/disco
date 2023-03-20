97b258548b6d1a619e773610ea655eade28c7fb2
Wang Yucai <netowls-studio@outlook.com>
2023-03-20 23:27:23 +0800

    feat: 新增断言工具方法。

---
+++ src/Disco.Common/Utilities/AssertUtilities.cs
@@ -0,0 +1,11 @@
+﻿// COPYRIGHT © 2023 WANG YUCAI. ALL RIGHTS RESERVED.
+//
+// LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+namespace Niacomsoft.Disco.Utilities
+{
+    /// <summary> 提供了断言相关的工具方法。 </summary>
+    public static class AssertUtilities
+    {
+    }
+}
