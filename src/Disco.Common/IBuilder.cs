e3e185d4086eb90e042642f7f23debf1e3efc15c
Wang Yucai <netowls-studio@outlook.com>
2023-03-23 21:52:58 +0800

    feat: 新增构建器抽象类型。

---
+++ src/Disco.Common/IBuilder.cs
@@ -0,0 +1,23 @@
+﻿// COPYRIGHT © 2023 WANG YUCAI. ALL RIGHTS RESERVED.
+//
+// LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+namespace Niacomsoft.Disco
+{
+    /// <summary>
+    /// 定义了构建 <typeparamref name="T" /> 类型的对象实例接口。
+    /// </summary>
+    /// <typeparam name="T">
+    /// 目标类型。
+    /// </typeparam>
+    public interface IBuilder<T> where T : class
+    {
+        /// <summary>
+        /// 构建 <typeparamref name="T" /> 类型的对象实例。
+        /// </summary>
+        /// <returns>
+        /// <typeparamref name="T" /> 类型的对象实例。
+        /// </returns>
+        T Build();
+    }
+}
