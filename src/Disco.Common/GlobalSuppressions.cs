78668211770ed8391f09f78a4843179d9de93237
Wang Yucai <netowls-studio@outlook.com>
2023-03-24 17:16:13 +0800

    refactor: 删除了不合理的代码。

--- src/Disco.Common/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Design", "Ex0100:Member may throw undocumented exception", Justification = "<挂起>", Scope = "member", Target = "~M:Niacomsoft.Disco.Diagnostics.TraceWriter.InternalWrite(Niacomsoft.Disco.Diagnostics.TraceLevel,System.String)")]
