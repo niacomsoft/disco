ec39a452a68f757c617e4706efd09e7059b1d700
Wang Yucai <netowls-studio@outlook.com>
2023-03-26 16:18:37 +0800

    feat: 新增异常描述接口。

---
+++ src/Disco.Common/Infrastructures/ExceptionDescriptors.cs
@@ -0,0 +1,38 @@
+﻿// COPYRIGHT © 2023 WANG YUCAI. ALL RIGHTS RESERVED. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+namespace Niacomsoft.Disco
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    ///     实现了 <see cref="IExceptionDescriptor" /> 类型接口的对象实例的集合。
+    ///     <para> 密闭的，不可以从此类型派生。 </para>
+    /// </summary>
+    /// <seealso cref="IList{T}" />
+    /// <seealso cref="List{T}" />
+    /// <seealso cref="IExceptionDescriptor" />
+    /// <remarks>
+    ///     密闭的，不可以从此类型派生。
+    /// </remarks>
+    public sealed class ExceptionDescriptors : List<IExceptionDescriptor>, IList<IExceptionDescriptor>
+    {
+        /// <summary>
+        ///     用于初始化一个 <see cref="ExceptionDescriptors" /> 类型的对象实例。
+        /// </summary>
+        public ExceptionDescriptors()
+        {
+        }
+
+        /// <summary>
+        ///     用于初始化一个 <see cref="ExceptionDescriptors" /> 类型的对象实例。
+        /// </summary>
+        /// <param name="collection">
+        ///     用于初始化的实现了 <see cref="IExceptionDescriptor" /> 类型接口的对象实例集合。
+        /// </param>
+        /// <seealso cref="IEnumerable{T}" />
+        /// <seealso cref="IExceptionDescriptor" />
+        public ExceptionDescriptors(IEnumerable<IExceptionDescriptor> collection) : base(collection)
+        {
+        }
+    }
+}
---
+++ src/Disco.Common/Infrastructures/IExceptionDescriptor.cs
@@ -0,0 +1,64 @@
+﻿// COPYRIGHT © 2023 WANG YUCAI. ALL RIGHTS RESERVED. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+namespace Niacomsoft.Disco
+{
+    using System;
+
+    /// <summary>
+    ///     定义了描述 <see cref="Exception" /> 类型的对象实例的接口。
+    /// </summary>
+    public interface IExceptionDescriptor
+    {
+        /// <summary>
+        ///     是否包含 <see cref="Exception.InnerException" /> 属性值。
+        /// </summary>
+        /// <value>
+        ///     设置或获取一个 <see cref="bool" /> 类型值，用于表示是否包含 <see cref="Exception.InnerException" /> 属性值。
+        /// </value>
+        /// <seealso cref="Exception" />
+        /// <seealso cref="Exception.InnerException" />
+        bool HasInnerExceptions { get; set; }
+
+        /// <summary>
+        ///     异常描述集合。
+        /// </summary>
+        /// <value>
+        ///     设置或获取 <see cref="ExceptionDescriptors" /> 类型的对象实例，用于表示异常描述集合。
+        /// </value>
+        /// <seealso cref="ExceptionDescriptors" />
+        ExceptionDescriptors InnerExceptions { get; set; }
+
+        /// <summary>
+        ///     包含程序集、命名空间的异常类型完全限定名称。
+        /// </summary>
+        /// <value>
+        ///     设置或获取一个字符串，用于表示包含程序集、命名空间的异常类型完全限定名称。
+        /// </value>
+        string QualifiedTypeName { get; set; }
+
+        /// <summary>
+        ///     异常堆栈信息。
+        /// </summary>
+        /// <value>
+        ///     设置或获取一个字符串，用于表示异常堆栈信息。
+        /// </value>
+        string StackTrace { get; set; }
+
+        /// <summary>
+        ///     引发异常的目标信息。
+        /// </summary>
+        /// <value>
+        ///     设置或获取 <see cref="IExceptionTargetSiteDescriptor" /> 类型的对象实例，用于表示引发异常的目标信息。
+        /// </value>
+        /// <seealso cref="IExceptionTargetSiteDescriptor" />
+        IExceptionTargetSiteDescriptor TargetSite { get; set; }
+
+        /// <summary>
+        ///     异常类型名称。
+        /// </summary>
+        /// <value>
+        ///     设置或获取一个字符串，用于表示异常类型名称。
+        /// </value>
+        string TypeName { get; set; }
+    }
+}
