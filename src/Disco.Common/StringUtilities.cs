94fbe55d559db0c448022b56c08d7bf68847599e
Wang Yucai <netowls-studio@outlook.com>
2023-03-27 22:50:17 +0800

    feat: 修改了 StringUtilities，增加了 GetBytes 方法。

--- src/Disco.Common/StringUtilities.cs
+++ src/Disco.Common/StringUtilities.cs
@@ -3,12 +3,50 @@
 namespace Niacomsoft.Disco
 {
     using System;
+    using System.Text;
 
     /// <summary>
     ///     提供了 <see cref="String" /> 相关的工具方法。
     /// </summary>
     public static class StringUtilities
     {
+        /// <summary>
+        ///     使用编码 <paramref name="encoding" /> 获取字符串 <paramref name="s" /> 的字节数组。当 <paramref name="s" /> 等于 <c> null </c> 时，将返回 <c> null </c>。
+        /// </summary>
+        /// <param name="s">
+        ///     字符串。
+        /// </param>
+        /// <param name="encoding">
+        ///     编码程序。
+        ///     <para> 派生自 <see cref="Encoding" /> 类型的对象实例。 </para>
+        /// </param>
+        /// <returns>
+        ///     字节数组。
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///     当 <paramref name="encoding" /> 等于 <c> null </c> 时，将引发此类型的异常。
+        /// </exception>
+        /// <seealso cref="Encoding" />
+        public static byte[] GetBytes(string s, Encoding encoding)
+        {
+            ExceptionHelper.IfArgumentNull(encoding, nameof(encoding));
+            return s is null ? null : encoding.GetBytes(s);
+        }
+
+        /// <summary>
+        ///     使用 <see cref="UTF8Encoding" /> 编码获取字符串 <paramref name="s" /> 的字节数组。当 <paramref name="s" /> 等于 <c> null </c> 时，将返回 <c> null </c>。
+        /// </summary>
+        /// <param name="s">
+        ///     字符串。
+        /// </param>
+        /// <returns>
+        ///     字节数组。
+        /// </returns>
+        /// <seealso cref="UTF8Encoding" />
+        /// <seealso cref="Encoding.UTF8" />
+        /// <seealso cref="StringUtilities.GetBytes(string, Encoding)" />
+        public static byte[] GetBytes(string s) => StringUtilities.GetBytes(s, Encoding.UTF8);
+
         /// <summary>
         ///     当 <paramref name="s" /> 等于 <c> null </c> 或 <see cref="string.Empty" /> 时，返回 <paramref name="default" />；否则返回 <paramref name="s" />。
         /// </summary>
