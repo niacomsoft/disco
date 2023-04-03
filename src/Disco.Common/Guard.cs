f652a9d454335bde1e96874ad248df42a60777bf
Wang Yucai <netowls-studio@outlook.com>
2023-04-03 22:20:50 +0800

    feat: 新增字符串守卫方法 Guard.SafeGet。

---
+++ src/Disco.Common/Guard.cs
@@ -0,0 +1,13 @@
+﻿// Copyright © 2006 - 2023 Wang Yucai. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Niacomsoft.Disco
+{
+    /// <summary>
+    ///     提供了守卫相关的方法。
+    /// </summary>
+    public static partial class Guard
+    {
+    }
+}
