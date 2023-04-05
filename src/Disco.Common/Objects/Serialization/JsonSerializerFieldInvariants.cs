373f9166819e0c7cedb6e263cd2a6aa6e71aee40
Wang Yucai <netowls-studio@outlook.com>
2023-04-05 16:40:45 +0800

    feat: 新增名称接口。

--- src/Disco.Common/Objects/Serialization/JsonSerializerFieldInvariants.cs
+++ src/Disco.Common/Objects/Serialization/JsonSerializerFieldInvariants.cs
@@ -15,9 +15,24 @@ namespace Niacomsoft.Disco.Objects.Serialization
         public const string Code = "code";
 
         /// <summary>
+        ///     显示名称字段名称。
+        /// </summary>
+        public const string DisplayName = "displayName";
+
+        /// <summary>
+        ///     友好名称字段名称。
+        /// </summary>
+        public const string FriendlyName = "friendlyName";
+
+        /// <summary>
         ///     标识字段名称。
         /// </summary>
         public const string Id = "id";
+
+        /// <summary>
+        ///     名称字段名称。
+        /// </summary>
+        public const string Name = "name";
 
         /// <summary>
         ///     开放标识字段名称。
