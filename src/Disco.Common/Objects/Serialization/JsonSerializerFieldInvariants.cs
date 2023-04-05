40625e745693e402625a9d16de7e99d818a0a164
Wang Yucai <netowls-studio@outlook.com>
2023-04-05 16:30:47 +0800

    feat: 新增开放标识接口。

---
+++ src/Disco.Common/Objects/Serialization/JsonSerializerFieldInvariants.cs
@@ -0,0 +1,22 @@
+﻿// Copyright © 2006 - 2023 Wang Yucai. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Niacomsoft.Disco.Objects.Serialization
+{
+    /// <summary>
+    ///     提供了 JSON 序列化字段相关的常量。
+    /// </summary>
+    public static class JsonSerializerFieldInvariants
+    {
+        /// <summary>
+        ///     标识字段名称。
+        /// </summary>
+        public const string Id = "id";
+
+        /// <summary>
+        ///     开放标识字段名称。
+        /// </summary>
+        public const string OpenId = "openId";
+    }
+}
