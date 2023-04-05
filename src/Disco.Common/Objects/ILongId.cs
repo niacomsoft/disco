65beec9de92c9131f75d1fe78ef769d435349cf7
Wang Yucai <netowls-studio@outlook.com>
2023-04-05 16:25:12 +0800

    feat: 新增标识接口。

---
+++ src/Disco.Common/Objects/ILongId.cs
@@ -0,0 +1,13 @@
+﻿// Copyright © 2006 - 2023 Wang Yucai. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Niacomsoft.Disco.Objects
+{
+    /// <summary>
+    ///     定义了 <see cref="long" /> 类型的标识接口。
+    /// </summary>
+    public interface ILongId : IId<long>
+    {
+    }
+}
---
+++ src/Disco.Common/Objects/IStringifyId.cs
@@ -0,0 +1,14 @@
+﻿// Copyright © 2006 - 2023 Wang Yucai. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Niacomsoft.Disco.Objects
+{
+    /// <summary>
+    ///     定义了 <see cref="string" /> 类型的标识接口。
+    /// </summary>
+    /// <seealso cref="IId{T}" />
+    public interface IStringifyId : IId<string>
+    {
+    }
+}
