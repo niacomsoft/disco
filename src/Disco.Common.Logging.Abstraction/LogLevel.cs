cbd61c6c80b69453684dcf3cebd281b50d37c65d
Wang Yucai <netowls-studio@outlook.com>
2023-04-12 22:17:11 +0800

    feat: 新增日志级别枚举类型。

---
+++ src/Disco.Common.Logging.Abstraction/LogLevel.cs
@@ -0,0 +1,60 @@
+﻿// Copyright © 2006 - 2023 Wang Yucai. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Niacomsoft.Disco.Logging
+{
+    using System;
+
+    /// <summary>
+    ///     定义了日志级别枚举类型。
+    /// </summary>
+    /// <seealso cref="FlagsAttribute" />
+    [Flags]
+    public enum LogLevel
+    {
+        /// <summary>
+        ///     追踪级。
+        /// </summary>
+        Trace = 1,
+
+        /// <summary>
+        ///     调试级。
+        /// </summary>
+        Debug = 2,
+
+        /// <summary>
+        ///     信息级。
+        /// </summary>
+        Information = 4,
+
+        /// <summary>
+        ///     警告级。
+        /// </summary>
+        Warning = 8,
+
+        /// <summary>
+        ///     错误级。
+        /// </summary>
+        Error = 16,
+
+        /// <summary>
+        ///     致命的。
+        /// </summary>
+        Fatal = 32,
+
+#if DEBUG
+
+        /// <summary>
+        ///     默认的（等效于 <see cref="Debug" />）。
+        /// </summary>
+        Default = Debug
+
+#else
+        /// <summary>
+        ///     默认的（等效于 <see cref="Information" />）。
+        /// </summary>
+        Default = Information
+#endif
+    }
+}
