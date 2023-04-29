23cc646be238c0160f90f4b38ba17ef9dc5adcd6
Wang Yucai <netowls-studio@outlook.com>
2023-04-29 15:53:58 +0800

    feat: 新增文化区域变更事件委托。

---
+++ src/Disco.Common/Globalization/CultureChangeEventArgs.cs
@@ -0,0 +1,38 @@
+﻿// Copyright © 2006 - 2023 Wang Yucai. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Niacomsoft.Disco.Globalization
+{
+    using System;
+    using System.Globalization;
+
+    /// <summary>
+    ///     提供了文化区域变更事件相关的参数。密闭的，不可以从此类型派生。
+    /// </summary>
+    /// <seealso cref="EventArgs" />
+    public sealed class CultureChangeEventArgs : EventArgs
+    {
+        /// <summary>
+        ///     初始化 <see cref="CultureChangeEventArgs" /> 的新实例。
+        /// </summary>
+        /// <param name="currentCulture">
+        ///     切换到的新的文化区域信息。
+        /// </param>
+        /// <seealso cref="CultureInfo" />
+        /// <seealso cref="Guard.SafeGet{T}(T, ConstructorDelegation{T}, object[])" />
+        /// <seealso cref="LocalizationDefaults" />
+        /// <seealso cref="LocalizationDefaults.Culture" />
+        public CultureChangeEventArgs(CultureInfo currentCulture) 
+            => CurrentCulture = Guard.SafeGet(currentCulture, (args) => LocalizationDefaults.Culture);
+
+        /// <summary>
+        ///     切换到的新的文化区域信息。
+        /// </summary>
+        /// <value>
+        ///     获取 <see cref="CultureInfo" /> 类型的对象实例，用于表示切换到的新的文化区域信息。
+        /// </value>
+        /// <seealso cref="CultureInfo" />
+        public CultureInfo CurrentCulture { get; }
+    }
+}
---
+++ src/Disco.Common/Globalization/CultureChangeEventHandler.cs
@@ -0,0 +1,19 @@
+﻿// Copyright © 2006 - 2023 Wang Yucai. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Niacomsoft.Disco.Globalization
+{
+    /// <summary>
+    ///     定义了处理文化区域变更事件的委托。
+    /// </summary>
+    /// <param name="sender">
+    ///     触发此事件的对象实例。
+    /// </param>
+    /// <param name="e">
+    ///     文化区域变更事件参数。
+    ///     <para> <see cref="CultureChangeEventArgs" /> 类型的对象实例。 </para>
+    /// </param>
+    /// <seealso cref="CultureChangeEventArgs" />
+    public delegate void CultureChangeEventHandler(object sender, CultureChangeEventArgs e);
+}
