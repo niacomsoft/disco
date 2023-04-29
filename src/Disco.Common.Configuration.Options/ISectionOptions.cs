db9d690586553a94f4dae1b62ff8b6e9c1bac5d8
Wang Yucai <netowls-studio@outlook.com>
2023-04-29 15:06:39 +0800

    feat: 新增配置选项标记接口。

---
+++ src/Disco.Common.Configuration.Options/ISectionOptions.cs
@@ -0,0 +1,14 @@
+﻿// Copyright © 2006 - 2023 Wang Yucai. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Niacomsoft.Disco.Configuration.Options
+{
+    /// <summary>
+    ///     定义了配置选项标记接口。
+    ///     <para> 此接口无实际操作，仅用于标记配置选项对象。 </para>
+    /// </summary>
+    public interface ISectionOptions
+    {
+    }
+}
