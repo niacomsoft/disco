35db38dfa85a2ac7e12433c6b0bf9ee0533419d2
Wang Yucai <netowls-studio@outlook.com>
2023-04-29 15:27:01 +0800

    feat: 新增本地化默认值。

---
+++ src/Disco.Common/Globalization/LocalizationDefaults.cs
@@ -0,0 +1,26 @@
+﻿// Copyright © 2006 - 2023 Wang Yucai. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Niacomsoft.Disco.Globalization
+{
+    using System.Globalization;
+
+    /// <summary>
+    ///     提供了本地化默认值相关的方法。
+    /// </summary>
+    public static class LocalizationDefaults
+    {
+        /// <summary>
+        ///     默认的文化区域名称。
+        /// </summary>
+        public const string CultureName = "zh-CN";
+
+        /// <summary>
+        ///     默认的文化区域信息。
+        ///     <para> <see cref="CultureInfo" /> 类型的对象实例。 </para>
+        /// </summary>
+        /// <seealso cref="CultureInfo" />
+        public static readonly CultureInfo Culture = new CultureInfo(CultureName);
+    }
+}
