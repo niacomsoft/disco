5d81fc9698332a08e898a15789526c8dacd4991b
Wang Yucai <netowls-studio@outlook.com>
2023-04-30 18:46:18 +0800

    feat: 新增 SMTP 协议默认值。

---
+++ src/Disco.Common/Net/SmtpProtocols/SmtpProtocolDefaults.cs
@@ -0,0 +1,26 @@
+﻿// Copyright © 2006 - 2023 Wang Yucai. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Niacomsoft.Disco.Net.SmtpProtocols
+{
+    /// <summary>
+    ///     提供了 SMTP 通信协议相关的默认值。
+    /// </summary>
+    public static class SmtpProtocolDefaults
+    {
+        /// <summary>
+        ///     默认的 SMTP 通信协议端口号。
+        /// </summary>
+        /// <seealso cref="INetworkCommunicationPort" />
+        /// <seealso cref="NetworkCommunicationPort.Create(int)" />
+        public static readonly INetworkCommunicationPort DefaultPort = NetworkCommunicationPort.Create(25);
+
+        /// <summary>
+        ///     支持 SSL 的 SMTP 通信协议端口号。
+        /// </summary>
+        /// <seealso cref="INetworkCommunicationPort" />
+        /// <seealso cref="NetworkCommunicationPort.Create(int)" />
+        public static readonly INetworkCommunicationPort SslProtocolPort = NetworkCommunicationPort.Create(465);
+    }
+}
