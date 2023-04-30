68c009d21160edf2105e1ca8dc85b2c26ac496be
Wang Yucai <netowls-studio@outlook.com>
2023-04-30 17:38:36 +0800

    feat: 新增空白字符串对比方法。

---
+++ src/Disco.Common/EmptyStringCompareOptions.cs
@@ -0,0 +1,32 @@
+﻿// Copyright © 2006 - 2023 Wang Yucai. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Niacomsoft.Disco
+{
+    /// <summary>
+    ///     定义了空字符串对比选项枚举。
+    /// </summary>
+    public enum EmptyStringCompareOptions
+    {
+        /// <summary>
+        ///     仅对比字符串是否为 <c> null </c> 值。
+        /// </summary>
+        OnlyNull = 1,
+
+        /// <summary>
+        ///     对比字符串是否为 <c> null </c> 或 <see cref="string.Empty" />。
+        /// </summary>
+        NullOrEmpty = 2,
+
+        /// <summary>
+        ///     对比字符串是否为 <c> null </c>、 <see cref="string.Empty" /> 或空白符。
+        /// </summary>
+        NullOrWhitespace = 3,
+
+        /// <summary>
+        ///     默认的对比选项。等同于 <see cref="NullOrEmpty" />。
+        /// </summary>
+        Default = NullOrEmpty
+    }
+}
---
+++ src/Disco.Common/StringEqualityComparer.cs
@@ -0,0 +1,82 @@
+﻿// Copyright © 2006 - 2023 Wang Yucai. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Niacomsoft.Disco
+{
+#if NET20 || NET30 || NET35
+    using System.Text.RegularExpressions;
+#endif
+
+    /// <summary>
+    ///     提供了字符串对比相关的方法。
+    /// </summary>
+    public static class StringEqualityComparer
+    {
+#if NET20 || NET30 || NET35
+        /// <summary>
+        ///     校验字符串 <paramref name="s" /> 是否全为空白符。
+        /// </summary>
+        /// <param name="s">
+        ///     需要校验的字符串。
+        /// </param>
+        /// <returns>
+        ///     如果字符串全部为空白符，则返回 <c> true </c>；否则返回 <c> false </c>。
+        /// </returns>
+        private static bool IsWhiteSpace(string s)
+        {
+            return string.IsNullOrEmpty(s) || Regex.IsMatch(s, @"^\s*$", RegexOptions.Multiline | RegexOptions.Singleline);
+        }
+#endif
+
+
+#if NET20 || NET30 || NET35
+        /// <summary>
+        ///     用于对比字符串 <paramref name="s" /> 是否等于 <c> null </c>、 <see cref="string.Empty" /> 或空格符。
+        /// </summary>
+        /// <param name="s">
+        ///     需要判断的字符串。
+        /// </param>
+        /// <param name="options">
+        ///     <see cref="EmptyStringCompareOptions" /> 类型的值。
+        /// </param>
+        /// <returns>
+        ///     当 <paramref name="s" /> 等于 <c> null </c>、 <see cref="string.Empty" /> 或空格符时，返回 <c> true </c>；否则返回 <c> false </c>。
+        /// </returns>
+        /// <seealso cref="EmptyStringCompareOptions" />
+        /// <seealso cref="ReferenceEqualityComparer"/>
+        /// <seealso cref="string.IsNullOrEmpty(string)"/>
+        /// <seealso cref="IsWhiteSpace(string)"/>
+#else
+        /// <summary>
+        ///     用于对比字符串 <paramref name="s" /> 是否等于 <c> null </c>、 <see cref="string.Empty" /> 或空格符。
+        /// </summary>
+        /// <param name="s">
+        ///     需要判断的字符串。
+        /// </param>
+        /// <param name="options">
+        ///     <see cref="EmptyStringCompareOptions" /> 类型的值。
+        /// </param>
+        /// <returns>
+        ///     当 <paramref name="s" /> 等于 <c> null </c>、 <see cref="string.Empty" /> 或空格符时，返回 <c> true </c>；否则返回 <c> false </c>。
+        /// </returns>
+        /// <seealso cref="EmptyStringCompareOptions" />
+        /// <seealso cref="ReferenceEqualityComparer"/>
+        /// <seealso cref="string.IsNullOrEmpty(string)"/>
+        /// <seealso cref="string.IsNullOrWhiteSpace(string)"/>
+#endif
+        public static bool IsNullOrWhitespace(string s, EmptyStringCompareOptions options = EmptyStringCompareOptions.Default)
+        {
+            if (options == EmptyStringCompareOptions.OnlyNull) return ReferenceEqualityComparer.IsNull(s);
+            else if (options == EmptyStringCompareOptions.NullOrEmpty) return string.IsNullOrEmpty(s);
+            else
+            {
+#if NET20 || NET30 || NET35
+                return IsWhiteSpace(s);
+#else
+                return string.IsNullOrWhiteSpace(s);
+#endif
+            }
+        }
+    }
+}
