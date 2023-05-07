a34bf1ffdda54fe6ea0c7173762e99db617c76dc
Wang Yucai <netowls-studio@outlook.com>
2023-05-07 14:15:40 +0800

    feat: 新增密钥生成策略枚举。( issue #19 )

---
+++ src/Disco.Common/Security/SecretKeyGeneratePolicy.cs
@@ -0,0 +1,30 @@
+﻿// Copyright © 2006 - 2023 Wang Yucai. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Niacomsoft.Disco.Security
+{
+    using System;
+
+    /// <summary>
+    ///     定义了密钥生成策略枚举类型。
+    /// </summary>
+    [Flags]
+    public enum SecretKeyGeneratePolicy
+    {
+        /// <summary>
+        ///     是否包含数字。
+        /// </summary>
+        IncludeNumbers = 1,
+
+        /// <summary>
+        ///     是否包含英文字母。
+        /// </summary>
+        IncludeLetters = 2,
+
+        /// <summary>
+        ///     是否包含特殊字符。
+        /// </summary>
+        IncludeSymbols = 4
+    }
+}
--- src/Disco.Common/Security/SecretKeySalt.cs
+++ src/Disco.Common/Security/SecretKeySalt.cs
@@ -4,12 +4,35 @@
 
 namespace Niacomsoft.Disco.Security
 {
+    using System.Text;
+
     /// <summary>
     ///     提供了生成随机密钥种子相关的方法。
     /// </summary>
     /// <seealso cref="ISecretKeySalt" />
-    public abstract class SecretKeySalt : ISecretKeySalt
+    public class SecretKeySalt : ISecretKeySalt
     {
+        /// <summary>
+        ///     区分大小写的英文字母密钥种子。
+        /// </summary>
+        /// <seealso cref="ISecretKeySalt" />
+        /// <seealso cref="LetterSalt" />
+        public static readonly ISecretKeySalt LetterSalt = new LetterSalt(false);
+
+        /// <summary>
+        ///     数字密钥种子。
+        /// </summary>
+        /// <seealso cref="ISecretKeySalt" />
+        /// <seealso cref="NumberSalt" />
+        public static readonly ISecretKeySalt NumberSalt = new NumberSalt();
+
+        /// <summary>
+        ///     特殊字符密钥种子。
+        /// </summary>
+        /// <seealso cref="ISecretKeySalt" />
+        /// <seealso cref="SymbolSalt" />
+        public static readonly ISecretKeySalt SymbolSalt = new SymbolSalt();
+
         /// <summary>
         ///     初始化 <see cref="SecretKeySalt" /> 的新实例。
         /// </summary>
@@ -28,5 +51,43 @@ namespace Niacomsoft.Disco.Security
         ///     获取一个字符串，用于表示随机密钥种子字符串。
         /// </value>
         public virtual string Value { get; }
+
+        /// <summary>
+        ///     使用指定策略生成新的密钥种子。
+        /// </summary>
+        /// <param name="policy">
+        ///     密钥生成策略。
+        ///     <para> <see cref="SecretKeyGeneratePolicy" /> 中的一个或几个值。 </para>
+        /// </param>
+        /// <returns>
+        ///     实现了 <see cref="ISecretKeySalt" /> 类型接口的对象实例。
+        /// </returns>
+        /// <seealso cref="ISecretKeySalt" />
+        /// <seealso cref="SecretKeyGeneratePolicy" />
+        public static ISecretKeySalt New(SecretKeyGeneratePolicy policy) 
+            => new SecretKeySalt(GetSaltStringBuilder(policy).ToString());
+
+        /// <summary>
+        ///     获取密钥种子字符串。
+        /// </summary>
+        /// <param name="policy">
+        ///     <see cref="SecretKeyGeneratePolicy" /> 中的一个或几个值。
+        /// </param>
+        /// <returns>
+        ///     <see cref="StringBuilder" /> 类型的对象实例。
+        /// </returns>
+        /// <seealso cref="StringBuilder" />
+        /// <seealso cref="SecretKeyGeneratePolicy" />
+        private static StringBuilder GetSaltStringBuilder(SecretKeyGeneratePolicy policy)
+        {
+            var saltBuilder = new StringBuilder();
+            if ((policy & SecretKeyGeneratePolicy.IncludeNumbers) == SecretKeyGeneratePolicy.IncludeNumbers)
+                saltBuilder.Append(NumberSalt.Value);
+            if ((policy & SecretKeyGeneratePolicy.IncludeLetters) == SecretKeyGeneratePolicy.IncludeLetters)
+                saltBuilder.Append(LetterSalt.Value);
+            if ((policy & SecretKeyGeneratePolicy.IncludeSymbols) == SecretKeyGeneratePolicy.IncludeSymbols)
+                saltBuilder.Append(SymbolSalt.Value);
+            return saltBuilder;
+        }
     }
 }
