0f7a7879d743057b7dff5ef84627845ec9fff7b6
Wang Yucai <netowls-studio@outlook.com>
2024-09-26 22:45:19 +0800

    feat: 新增断言工具类库。

---
+++ src/disco-fx/Disco.Utilities/AssertUtilities.Conditional.cs
@@ -0,0 +1,26 @@
+﻿/* ********************************************************************************************************************************************************** *\
+ * COPYRIGHT © 2006 - 2024 WANG YUCAI. ALL RIGHTS RESERVED.                                                                                                   *
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                                                         *
+\* ********************************************************************************************************************************************************** */
+
+namespace Disco
+{
+  public static partial class AssertUtilities
+  {
+#if DEBUG
+
+    /// <summary>
+    ///   是否为调试模式。
+    /// </summary>
+    public static readonly bool DebugMode = true;
+
+#else
+
+    /// <summary>
+    ///   是否为调试模式。
+    /// </summary>
+    public static readonly bool DebugMode = false;
+
+#endif
+  }
+}
---
+++ src/disco-fx/Disco.Utilities/AssertUtilities.Int32.cs
@@ -0,0 +1,60 @@
+﻿/* ********************************************************************************************************************************************************** *\
+ * COPYRIGHT © 2006 - 2024 WANG YUCAI. ALL RIGHTS RESERVED.                                                                                                   *
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                                                         *
+\* ********************************************************************************************************************************************************** */
+
+namespace Disco
+{
+  public static partial class AssertUtilities
+  {
+    #region Methods
+
+    /// <summary>
+    ///   当 <c> <paramref name="a" /> = <paramref name="b" /></c> 时，返回 <see langword="true" />；否则返回 <see langword="false" /> 。
+    /// </summary>
+    /// <param name="a">
+    ///   <see cref="int" /> 类型的值。
+    /// </param>
+    /// <param name="b">
+    ///   <see cref="int" /> 类型的值。
+    /// </param>
+    /// <returns>
+    ///   当 <c> <paramref name="a" /> = <paramref name="b" /></c> 时，返回 <see langword="true" />；否则返回 <see langword="false" /> 。
+    /// </returns>
+    public static bool Equals(int a, int b = 0) => a == b;
+
+    /// <summary>
+    ///   当 <c> <paramref name="a" /> &gt; <paramref name="b" /></c> 时，返回 <see langword="true" />；否则返回 <see langword="false" />
+    ///   。
+    /// </summary>
+    /// <param name="a">
+    ///   <see cref="int" /> 类型的值。
+    /// </param>
+    /// <param name="b">
+    ///   <see cref="int" /> 类型的值。
+    /// </param>
+    /// <returns>
+    ///   当 <c> <paramref name="a" /> &gt; <paramref name="b" /></c> 时，返回 <see langword="true" />；否则返回 <see langword="false" />
+    ///   。
+    /// </returns>
+    public static bool GreaterThan(int a, int b = 0) => a > b;
+
+    /// <summary>
+    ///   当 <c> <paramref name="a" /> &gt;= <paramref name="b" /></c> 时，返回 <see langword="true" />；否则返回 <see langword="false" />
+    ///   。
+    /// </summary>
+    /// <param name="a">
+    ///   <see cref="int" /> 类型的值。
+    /// </param>
+    /// <param name="b">
+    ///   <see cref="int" /> 类型的值。
+    /// </param>
+    /// <returns>
+    ///   当 <c> <paramref name="a" /> &gt;= <paramref name="b" /></c> 时，返回 <see langword="true" />；否则返回 <see langword="false" />
+    ///   。
+    /// </returns>
+    public static bool GreaterThanOrEquals(int a, int b = 0) => a >= b;
+
+    #endregion Methods
+  }
+}
