bd2cf2f5e71d25f78afc450952b1ebac035589a2
Wang Yucai <netowls-studio@outlook.com>
2024-09-30 14:12:38 +0800

    feat: 新增异常助手类。

---
+++ src/disco-fx/Disco.Utilities/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Design", "Ex0100:Member may throw undocumented exception", Justification = "<挂起>", Scope = "member", Target = "~M:Disco.ExceptionHelper.ThrowIf``1(System.Boolean,System.Object[])")]
