389b245d0d4f8d51f3a768545de61f56f86028b0
Wang Yucai <netowls-studio@outlook.com>
2024-09-30 13:53:37 +0800

    feat: 新增 NotNull 扩展方法。
    修改了 ObjectExtensions，增加了 NotNull 扩展方法。

--- src/disco-fx/Disco.Utilities/ObjectExtensions.cs
+++ src/disco-fx/Disco.Utilities/ObjectExtensions.cs
@@ -5,6 +5,11 @@
 
 namespace Disco
 {
+  using Disco.Diagnostics;
+  using Disco.Resources;
+
+  using System;
+
   /// <summary>
   ///   为 <see cref="object" /> 类型提供了扩展方法。
   /// </summary>
@@ -24,6 +29,20 @@ namespace Disco
     public static bool IsNull(this object me)
     {
       return me is null;
+    }
+
+    /// <summary>
+    ///   当 <paramref name="me" /> 等于 <see langword="null" /> 时，返回 <see langword="false" />；否则返回 <see langword="true" /> 。
+    /// </summary>
+    /// <param name="me">
+    ///   <see cref="object" /> 类型的对象实例。
+    /// </param>
+    /// <returns>
+    ///   当 <paramref name="me" /> 等于 <see langword="null" /> 时，返回 <see langword="false" />；否则返回 <see langword="true" /> 。
+    /// </returns>
+    public static bool NotNull(this object me)
+    {
+      return !IsNull(me);
     }
 
     #endregion Methods
