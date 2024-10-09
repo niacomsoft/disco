fa2ee75a4d2c3726f8afef343b5de510b1b144e3
Wang Yucai <netowls-studio@outlook.com>
2024-10-09 22:20:31 +0800

    feat: 新增默认值静态类。

---
+++ src/disco-fx/Disco.Models.Abstractions/IDescription.cs
@@ -0,0 +1,29 @@
+﻿/* ********************************************************************************************************************************************************** *\
+ * COPYRIGHT © 2006 - 2024 WANG YUCAI. ALL RIGHTS RESERVED.                                                                                                   *
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                                                         *
+\* ********************************************************************************************************************************************************** */
+
+namespace Disco.Models
+{
+  #region Interfaces
+
+  /// <summary>
+  ///   提供了描述或备注信息的接口。
+  /// </summary>
+  public interface IDescription
+  {
+    #region Properties
+
+    /// <summary>
+    ///   描述信息。
+    /// </summary>
+    /// <value>
+    ///   设置或获取一个字符串，用于表示描述信息。
+    /// </value>
+    string Description { get; set; }
+
+    #endregion Properties
+  }
+
+  #endregion Interfaces
+}
---
+++ src/disco-fx/Disco.Utilities/DefaultValue.cs
@@ -0,0 +1,32 @@
+﻿/* ********************************************************************************************************************************************************** *\
+ * COPYRIGHT © 2006 - 2024 WANG YUCAI. ALL RIGHTS RESERVED.                                                                                                   *
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                                                         *
+\* ********************************************************************************************************************************************************** */
+
+namespace Disco
+{
+  /// <summary>
+  ///   提供了 Disco 默认值相关的静态 <see langword="static" /> 方法。
+  /// </summary>
+  public static class DefaultValue
+  {
+    #region Constants
+
+    /// <summary>
+    ///   与 <see langword="false" /> 等效的值。
+    /// </summary>
+    public const int FalseValue = 0;
+
+    /// <summary>
+    ///   <see langword="null" /> 等效的 <see cref="bool" /> 类型的值。
+    /// </summary>
+    public const bool NullValue = false;
+
+    /// <summary>
+    ///   与 <see langword="true" /> 等效的值。
+    /// </summary>
+    public const int TrueValue = 1;
+
+    #endregion Constants
+  }
+}
