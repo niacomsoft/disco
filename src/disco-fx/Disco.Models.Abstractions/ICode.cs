0f4df6c670aae1ea51eb00e8767d8d0ef1427462
Wang Yucai <netowls-studio@outlook.com>
2024-10-13 15:17:10 +0800

    feat: 基本完成了模型抽象。

---
+++ src/disco-fx/Disco.Models.Abstractions/ICode.cs
@@ -0,0 +1,29 @@
+﻿/* ********************************************************************************************************************************************************** *\
+ * COPYRIGHT © 2006 - 2024 WANG YUCAI. ALL RIGHTS RESERVED.                                                                                                   *
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                                                         *
+\* ********************************************************************************************************************************************************** */
+
+namespace Disco.Models
+{
+  #region Interfaces
+
+  /// <summary>
+  ///   提供了编码的接口。
+  /// </summary>
+  public interface ICode
+  {
+    #region Properties
+
+    /// <summary>
+    ///   编码。
+    /// </summary>
+    /// <value>
+    ///   设置或获取一个字符串，用于表示编码。
+    /// </value>
+    string Code { get; set; }
+
+    #endregion Properties
+  }
+
+  #endregion Interfaces
+}
