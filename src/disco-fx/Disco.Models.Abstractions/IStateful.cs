2b6e9fdbd7ed855043495bfeaf692620e40a2068
Wang Yucai <netowls-studio@outlook.com>
2024-10-13 15:04:16 +0800

    feat: 新增描述状态的接口集。

--- src/disco-fx/Disco.Models.Abstractions/IStateful.cs
+++ src/disco-fx/Disco.Models.Abstractions/IStateful.cs
@@ -8,6 +8,69 @@ namespace Disco.Models
   #region Interfaces
 
   /// <summary>
+  ///   提供了是否已经激活的接口。
+  /// </summary>
+  /// <typeparam name="T">
+  ///   描述是否已经激活的值类型。
+  /// </typeparam>
+  public interface IAvailable<T> where T : struct
+  {
+    #region Properties
+
+    /// <summary>
+    ///   是否已经激活。
+    /// </summary>
+    /// <value>
+    ///   设置或获取 <typeparamref name="T" /> 类型的值，用于表示是否已经激活。
+    /// </value>
+    T Available { get; set; }
+
+    #endregion Properties
+  }
+
+  /// <summary>
+  ///   提供了描述是否为内置的状态的接口。
+  /// </summary>
+  /// <typeparam name="T">
+  ///   描述是否为内置的状态的值类型。
+  /// </typeparam>
+  public interface IBuiltIn<T> where T : struct
+  {
+    #region Properties
+
+    /// <summary>
+    ///   是否为内置的。
+    /// </summary>
+    /// <value>
+    ///   设置或获取 <typeparamref name="T" /> 类型的值，用于表示是否为内置的。
+    /// </value>
+    T BuiltIn { get; set; }
+
+    #endregion Properties
+  }
+
+  /// <summary>
+  ///   提供了描述是否为默认的接口。
+  /// </summary>
+  /// <typeparam name="T">
+  ///   是否为默认的状态的值类型。
+  /// </typeparam>
+  public interface IDefault<T> where T : struct
+  {
+    #region Properties
+
+    /// <summary>
+    ///   是否为默认的。
+    /// </summary>
+    /// <value>
+    ///   设置或获取 <typeparamref name="T" /> 类型的值，用于表示是否为默认的。
+    /// </value>
+    T IsDefault { get; set; }
+
+    #endregion Properties
+  }
+
+  /// <summary>
   ///   提供了描述是否启用状态的接口。
   /// </summary>
   /// <typeparam name="T">
@@ -24,6 +87,48 @@ namespace Disco.Models
     ///   设置 <typeparamref name="T" /> 类型的值，用于表示是否启用。
     /// </value>
     T Enabled { get; set; }
+
+    #endregion Properties
+  }
+
+  /// <summary>
+  ///   提供了描述是否被冻结状态的接口。
+  /// </summary>
+  /// <typeparam name="T">
+  ///   描述是否被冻结状态的值类型。
+  /// </typeparam>
+  public interface IFrozen<T> where T : struct
+  {
+    #region Properties
+
+    /// <summary>
+    ///   是否已经被冻结。
+    /// </summary>
+    /// <value>
+    ///   设置或获取 <typeparamref name="T" /> 类型的值，用于表示是否已经被冻结。
+    /// </value>
+    T Frozen { get; set; }
+
+    #endregion Properties
+  }
+
+  /// <summary>
+  ///   提供了描述是否锁定状态的接口。
+  /// </summary>
+  /// <typeparam name="T">
+  ///   描述是否已经被锁定的值类型。
+  /// </typeparam>
+  public interface ILocked<T> where T : struct
+  {
+    #region Properties
+
+    /// <summary>
+    ///   是否已经锁定。
+    /// </summary>
+    /// <value>
+    ///   设置或获取 <typeparamref name="T" /> 类型的值，用于表示是否已经锁定。
+    /// </value>
+    T Locked { get; set; }
 
     #endregion Properties
   }
