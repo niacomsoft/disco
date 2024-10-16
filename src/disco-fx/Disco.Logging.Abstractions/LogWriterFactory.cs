7548626f825116e7e9069d8a24df92d407894bc8
Wang Yucai <netowls-studio@outlook.com>
2024-10-16 21:36:00 +0800

    feat: 新增记录运行时日志服务工厂方法抽象。

---
+++ src/disco-fx/Disco.Logging.Abstractions/LogWriterFactory.cs
@@ -0,0 +1,36 @@
+﻿/* ********************************************************************************************************************************************************** *\
+ * COPYRIGHT © 2006 - 2024 WANG YUCAI. ALL RIGHTS RESERVED.                                                                                                   *
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                                                         *
+\* ********************************************************************************************************************************************************** */
+
+namespace Disco.Logging
+{
+  /// <summary>
+  ///   提供了创建实现了 <see cref="ILogWriter" /> 类型接口的对象实例相关的抽象工厂 <see langword="abstract" /> 方法。
+  /// </summary>
+  public abstract class LogWriterFactory : ILogWriterFactory
+  {
+    #region Methods
+
+    /// <inheritdoc />
+    public abstract ILogWriter CreateInstance();
+
+    #endregion Methods
+  }
+
+  /// <summary>
+  ///   提供了创建实现了 <see cref="ILogWriter{TCategoryName}" /> 类型接口的对象实例相关的抽象工厂 <see langword="abstract" /> 方法。
+  /// </summary>
+  /// <typeparam name="TCategoryName">
+  ///   标识日志类别名称的类型。
+  /// </typeparam>
+  public abstract class LogWriterFactory<TCategoryName> : ILogWriterFactory<TCategoryName>
+  {
+    #region Methods
+
+    /// <inheritdoc />
+    public abstract ILogWriter<TCategoryName> CreateInstance();
+
+    #endregion Methods
+  }
+}
